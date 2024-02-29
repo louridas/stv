8b8f5a9002badd31762cdc63de19b76818075718
Kostas Papadimitriou <kpap@grnet.gr>
2024-02-29 13:35:08 +0200

    backport: quota_callback_fewer_constituencies

--- quota_callback_fewer_constituencies.py
+++ quota_callback_fewer_constituencies.py
@@ -3,25 +3,25 @@ from stv import DefaultQuotaCallback, Action, LOG_MESSAGE
 class QuotaCallback(DefaultQuotaCallback):
     """
     Quota callback for constituencies fewer than seats.
-    
+
     Quota callback that overrules the quota when the number of constituencies
-    is fewer than the number of seats. 
+    is fewer than the number of seats.
     """
 
     def __init__(self, seats, quota_limit, logger):
-        super().__init__(seats, quota_limit)
+        super().__init__(seats, quota_limit, logger)
         self.overruled = 0
 
     def __call__(self, candidate, constituency_map, elected_per_constituency):
-        quota_exceeded = super().__call__(candidate, 
+        quota_exceeded = super().__call__(candidate,
                                           constituency_map, elected_per_constituency)
         if not quota_exceeded:
             return False
         diff = self.seats - len(constituency_map)
-        if diff > 0 and self.overruled < diff: 
+        if diff > 0 and self.overruled < diff:
             self.overruled += 1
             d = ("Quota overruled. Constituencies fewer than seats.")
-            msg = LOG_MESSAGE.format(action=Action.COMMENT.value, desc=d)
+            msg = LOG_MESSAGE.format(action=Action.COMMENT, desc=d)
             self.logger.info(msg)
             return False
         return True
