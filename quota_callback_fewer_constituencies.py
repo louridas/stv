1646fa73e3ecc200e600020c0d32e85a7f992987
Panos Louridas <louridas@gmail.com>
2026-04-17 17:48:32 +0300

    fix diff calculation

--- quota_callback_fewer_constituencies.py
+++ quota_callback_fewer_constituencies.py
@@ -11,13 +11,16 @@ class QuotaCallback(DefaultQuotaCallback):
     def __init__(self, seats, quota_limit, logger):
         super().__init__(seats, quota_limit)
         self.overruled = 0
-
+        self.logger = logger
     def __call__(self, candidate, constituency_map, elected_per_constituency):
-        quota_exceeded = super().__call__(candidate, 
-                                          constituency_map, elected_per_constituency)
+        quota_exceeded = super().__call__(
+            candidate, 
+            constituency_map,
+            elected_per_constituency
+        )
         if not quota_exceeded:
             return False
-        diff = self.seats - len(constituency_map)
+        diff = self.seats - len(set(constituency_map.values()))
         if diff > 0 and self.overruled < diff: 
             self.overruled += 1
             d = ("Quota overruled. Constituencies fewer than seats.")
